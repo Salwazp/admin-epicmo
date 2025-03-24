070300d85d4981c3acc92d63ff655d4d10030dc0
Salwa <salwa28ap@gmail.com>
2025-03-24 13:30:34 +0700

    sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -29,10 +29,22 @@
                             <span class="menu-title text-truncate" data-i18n="Banner">Banner</span>
                         </a>
                     </li>
+                    <li class="nav-item {{ request()->routeIs('admin.event.*') ? 'active' : '' }}">
+                        <a class="d-flex align-items-center" href="{{ route('admin.event.index') }}">
+                            <i data-feather='circle'></i>
+                            <span class="menu-title text-truncate" data-i18n="Event">Event</span>
+                        </a>
+                    </li>
                     <li class="nav-item {{ request()->routeIs('admin.about-section.*') ? 'active' : '' }}">
                         <a class="d-flex align-items-center" href="{{ route('admin.about-section.index') }}">
                             <i data-feather='circle'></i>
                             <span class="menu-title text-truncate" data-i18n="About-Section">About</span>
+                        </a>
+                    </li>
+                    <li class="nav-item {{ request()->routeIs('admin.running_image.*') ? 'active' : '' }}">
+                        <a class="d-flex align-items-center" href="{{ route('admin.running_image.index') }}">
+                            <i data-feather='circle'></i>
+                            <span class="menu-title text-truncate" data-i18n="Clients">Clients</span>
                         </a>
                     </li>
                     <li class="nav-item {{ request()->routeIs('admin.gallery.*') ? 'active' : '' }}">
@@ -48,12 +60,6 @@
                         </a>
                     </li>
                 </ul>
-            </li>
-            <li class="nav-item {{ request()->routeIs('admin.about.*') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.about.index') }}">
-                    <i data-feather='book-open'></i>
-                    <span class="menu-title text-truncate" data-i18n="About">About</span>
-                </a>
             </li>
             <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather='image'></i><span class="menu-title text-truncate" data-i18n="Invoice">Media</span></a>
                 <ul class="menu-content">
@@ -104,30 +110,6 @@
                     </li>
                     
                 </ul>
-            </li>
-            <li class="nav-item {{ request()->routeIs('admin.contact.*') && Request::segment(3) != 'form'  ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.contact.index') }}">
-                    <i data-feather='phone'></i>
-                    <span class="menu-title text-truncate" data-i18n="Contact">Contact</span>
-                </a>
-            </li>
-            <li class="nav-item {{ request()->routeIs('admin.career.*') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.career.index') }}">
-                    <i data-feather='layout'></i>
-                    <span class="menu-title text-truncate" data-i18n="Career">Career</span>
-                </a>
-            </li>
-            <li class="nav-item {{ request()->routeIs('admin.preference.*') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.preference.index') }}">
-                    <i data-feather='settings'></i>
-                    <span class="menu-title text-truncate" data-i18n="Preference">Preference</span>
-                </a>
-            </li>
-            <li class="nav-item {{ request()->routeIs('admin.contact.form.*') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.contact.form.list') }}">
-                    <i data-feather='users'></i>
-                    <span class="menu-title text-truncate" data-i18n="Contact Form">Contact Form</span>
-                </a>
             </li>
         </ul>
     </div>
